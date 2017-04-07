11ac163148f6d2c716a47fc1f599af4d1a48568d
IGI-111 <igi.222@gmail.com>
2017-04-07 17:24:02 +0200

    formatting

--- src/daemon.js
+++ src/daemon.js
@@ -1,8 +1,8 @@
-const rp = require('request-promise')
+const rp = require('request-promise');
 
-const daemon = 'http://localhost:6060'
+const daemon = 'http://localhost:6060';
 
-module.exports = {add, seed, remove, list, info}
+module.exports = {add, seed, remove, list, info};
 
 function add (torrent) {
   return rp.put({
@@ -10,7 +10,7 @@ function add (torrent) {
     json: {
       'torrent': torrent
     }
-  })
+  });
 }
 
 function seed (path) {
@@ -19,17 +19,17 @@ function seed (path) {
     json: {
       'path': path
     }
-  })
+  });
 }
 
 function remove (torrent) {
-  return rp.delete(`${daemon}/torrent/${torrent}`)
+  return rp.delete(`${daemon}/torrent/${torrent}`);
 }
 
 function list () {
-  return rp.get(`${daemon}/torrent`).then((res) => JSON.parse(res))
+  return rp.get(`${daemon}/torrent`).then((res) => JSON.parse(res));
 }
 
 function info (torrent) {
-  return rp.get(`${daemon}/torrent/${torrent}`).then((res) => JSON.parse(res))
+  return rp.get(`${daemon}/torrent/${torrent}`).then((res) => JSON.parse(res));
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const daemon = require('./daemon')
-const humanizeDuration = require('humanize-duration')
-const prettySize = require('prettysize')
+const program = require('commander');
+const daemon = require('./daemon');
+const humanizeDuration = require('humanize-duration');
+const prettySize = require('prettysize');
+const path = require('path');
 
 program
   .command('add [torrents...]')
@@ -12,13 +13,13 @@ program
     torrents.forEach((arg) => {
       try {
         daemon.add(arg).then((res) => {
-          console.log('Added ' + res)
-        })
+          console.log('Added ' + res);
+        });
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
     })
-  )
+  );
 
 program
   .command('list')
@@ -28,15 +29,15 @@ program
       daemon.list().then((torrents) => {
         torrents.forEach((t) => {
           daemon.info(t).then((i) => {
-            console.log(`${(i.progress * 100).toFixed(0)}% ${i.infoHash} ${i.name}`)
-          })
-        })
-      })
+            console.log(`${(i.progress * 100).toFixed(0)}% ${i.infoHash} ${i.name}`);
+          });
+        });
+      });
     } catch (e) {
-      console.error(e.message)
-      process.exit(1)
+      console.error(e.message);
+      process.exit(1);
     }
-  })
+  });
 
 program
   .command('status [torrent]')
@@ -45,15 +46,15 @@ program
     try {
       daemon.info(torrent).then((i) => {
         if (program.debug) {
-          console.log(JSON.stringify(i, null, 2))
+          console.log(JSON.stringify(i, null, 2));
         } else {
-          prettyPrint(i)
+          prettyPrint(i);
         }
-      })
+      });
     } catch (err) {
-      console.error(err.message)
+      console.error(err.message);
     }
-  })
+  });
 
 program
   .command('remove [torrents...]')
@@ -61,38 +62,37 @@ program
   .action(torrents =>
     torrents.forEach((arg) => {
       try {
-        daemon.remove(arg).then(() => console.log('Removed ' + arg))
+        daemon.remove(arg).then(() => console.log('Removed ' + arg));
       } catch (err) {
-        console.error(err.message)
+        console.error(err.message);
       }
     })
-  )
+  );
 
 program
   .command('seed [folder]')
   .description('Seed the specified folder as a new torrent.')
   .action(folder => {
-
     try {
-      daemon.seed(path.normalize(folder)).then((res) => console.log('Seeding ' + res.infoHash))
+      daemon.seed(path.normalize(folder)).then((res) => console.log('Seeding ' + res.infoHash));
     } catch (err) {
-      console.error(err.message)
+      console.error(err.message);
     }
-  })
+  });
 
-program.parse(process.argv)
+program.parse(process.argv);
 
 if (program.args.length < 1) {
-  program.help()
+  program.help();
 }
 
 function prettyPrint (info) {
   let remaining = info.timeRemaining === 0 ? ''
-    : `${humanizeDuration(info.timeRemaining, { round: true })} remaining\n`
+    : `${humanizeDuration(info.timeRemaining, { round: true })} remaining\n`;
 
   console.log(`${info.name}
 ${info.infoHash}
 ${prettySize(info.downloaded)} / ${prettySize(info.length)}
 ${remaining}${info.numPeers} peers connected`
-  )
+  );
 }
